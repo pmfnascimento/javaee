71a783dae0ca7329f3cc544289f04ee1485ccc9a
Pedro Nascimento <pmfnascimento@gmail.com>
2025-12-10 00:32:23 +0000

    Hello World Servlet

---
+++ HelloWorldServlet/src/main/java/org/helloworldservlet/servlet/HelloWorld.java
@@ -0,0 +1,45 @@
+package org.helloworldservlet.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		double sqrtvalue = Math.sqrt(4.0);
+		
+		out.println("<html><body><h1 align='center'>Hello World Servlet</h1><br><p align='center'>The Square root of 4.0 is: " + sqrtvalue + "</p><br></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
